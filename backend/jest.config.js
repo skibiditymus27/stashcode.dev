4080664b4accfa28c9e1812b5d87356d0deaeebb
skibiditymus27 <doroszkotymek@gmail.com>
2026-04-13 17:33:53 +0200

    delated all

--- backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'node',
-  rootDir: '.',
-  testMatch: ['**/test/**/*.test.js'],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.js'],
-  setupFiles: ['<rootDir>/test/setupEnv.js']
-};
